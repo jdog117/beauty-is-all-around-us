0135d981550d3d50a20b8125e9e0a35a28a07ed6
jdog117 <jspcasey@gmail.com>
2024-05-10 15:21:34 -0700

    v1 :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Boosh from "@/public/142595010033.jpg";
+
+// set up blob storage and pull images from it
+// checkout a few other storages/backends like supa base
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                <Image src={Boosh} alt=""></Image>
+            </div>
         </main>
     );
 }
